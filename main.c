34641a107d67229f1a299a177fd9f64612d0136f
Петро <115140949+Next-day4@users.noreply.github.com>
2022-10-30 17:54:49 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,34 +2,262 @@
 #include <stdlib.h>
 #include <math.h>
 
+unsigned int method_choose( void );
+double function ( double x );
+void print_result (double a, double b, unsigned int n, double I);
+double left_rectangle_method ( double a, double b, unsigned int n);
+double right_rectangle_method ( double a, double b, unsigned int n);
+double trapezoid_method ( double a, double b, unsigned int n);
+double simpss_method ( double a, double b, unsigned int n);
+
 int main()
-
 {
- double x,y;
-  printf("Enter x:");
-  scanf("%lf", &x);
 
- if( x>= -10,5 && x < 8,125)
+unsigned int variant_of_method;
+double a;
+double b;
+unsigned int n, N;
+double I, I1, I2;
+const double e = 0.0001;
 
-  y =sqrt(abs(x+2+cos(x))/2);
+    printf("\nEnter the left boundary (a): ");
+    scanf("%lf", &a);
+    printf("\nEnter the right boundary (b): ");
+    scanf("%lf", &b);
+    printf("\nEnter the number of partition intervals (n): ");
+    scanf("%u", &n);
 
- else if ( x >= 10 && x < 15 )
+    variant_of_method = method_choose();
 
-  y =4;
+    switch(variant_of_method)
+    {
+    case 1:
 
- else if ( x >= 50 && x < 150 )
+    I = left_rectangle_method(a, b, n);
+    print_result(a, b, n, I);
 
-  y =4-4*x;
+    N = 0;
+    do
+    {
+    N = N + 2;
+    I1=left_rectangle_method(a, b, N);
+    I2=left_rectangle_method(a, b, N+2);
+    }
+    while( fabs(I2-I1) > e );
+    printf("\n\n============================================");
+    printf("\n N=%u", N);
+    printf("\n============================================");
+    printf("\n\nThe calculation of the integral by the Left Rectangles method is finished\n");
 
- else
+    break;
 
-  y =-1,5;
+    case 2:
 
-  system("cls");
+    I = right_rectangle_method(a, b, n);
+    print_result(a, b, n, I);
 
-  printf("x = %lf", x);
+    N = 0;
+    do
+    {
+    N = N + 2;
+    I1=left_rectangle_method(a, b, N);
+    I2=left_rectangle_method(a, b, N+2);
+            }
+    while( fabs(I2-I1) > e );
+    printf("\n\n============================================");
+    printf("\n N=%u", N);
+    printf("\n============================================");
+    printf("\n\nThe calculation of the integral by the Right Rectangles method is finished\n");
 
-  printf("\ny = %lf", y);
+    break;
 
- return 0;
+    case 3:
+
+    I = trapezoid_method(a, b, n);
+    print_result(a, b, n, I);
+
+    N = 0;
+    do
+    {
+    N = N + 2;
+    I1=left_rectangle_method(a, b, N);
+    I2=left_rectangle_method(a, b, N+2);
+    }
+    while( fabs(I2-I1) > e );
+    printf("\n\n============================================");
+    printf("\n N=%u", N);
+    printf("\n============================================");
+    printf("\n\nThe calculation of the integral by the Trapezoid method is finished\n");
+
+    break;
+
+
+    case 4:
+
+    I = simpss_method(a, b, n);
+    print_result(a, b, n, I);
+
+    N = 0;
+            do
+            {
+    N = N + 2;
+    I1=left_rectangle_method(a, b, N);
+    I2=left_rectangle_method(a, b, N+2);
+    }
+    while( fabs(I2-I1) > e );
+    printf("\n\n============================================");
+    printf("\n N=%u", N);
+    printf("\n============================================");
+    printf("\n\nThe calculation of the integral by the Simps's method is finished\n");
+
+    break;
+
+    }
+
+
+
+    return 0;
 }
+
+
+//================================================================================
+//Вибір методу
+
+    unsigned int method_choose( void )
+{
+    unsigned int var;
+
+    system("color 2");
+    printf("\nChoose the method of calculating:");
+    printf("\n\n\t1.By Left Rectangles method.");
+    printf("\n\t2.By Right Rectangles method.");
+    printf("\n\t3.By Trapezoid method.");
+    printf("\n\t4.By Simps's method.\n");
+    scanf("%u", &var);
+
+    while( var < 1 || var > 4 )
+    {
+    printf("\n!!!! Invalid variant of method !!!!\n");
+    printf("\nChoose from the variant list:");
+    scanf("%u", &var);
+    }
+
+    return var;
+}
+
+//=================================================================================
+//Функція для якох обчислюється значення інтегралу
+    double function ( double x )
+{
+    double y;
+
+    y = pow(x,4) + 3*x;
+
+    return y;
+}
+
+//=================================================================================
+//Обрахунок інтегралу за методом лівих прямокутників
+
+    double left_rectangle_method ( double a, double b, unsigned int n)
+{
+    double h;
+    double x;
+    double sum = 0;
+
+    h = ( b - a ) / n;
+    x = a;
+
+    for ( int k = 0; k <= n-1; k++)
+    {
+      sum = sum + function(x);
+      x = x + h;
+    }
+
+    return sum * h;
+}
+
+//==================================================================================
+//Обрахунок інтегралу за методом правих прямокутників
+
+double right_rectangle_method ( double a, double b, unsigned int n)
+{
+    double h;
+    double x;
+    double sum = 0;
+
+    h = ( b - a ) / n;
+    x = a;
+
+    for ( int k = 1; k <= n; k++)
+    {
+      sum = sum + function(x);
+      x = x + h;
+    }
+
+    return sum * h;
+}
+
+//=================================================================================
+//Обрахунок інтегралу за методом трапецій
+
+double trapezoid_method ( double a, double b, unsigned int n)
+{
+    double h;
+    double x;
+    double sum = 0;
+
+    h = ( b - a ) / n;
+    x = a;
+
+    for ( int k = 1; k <= n-1; k++)
+    {
+      sum = sum + (function(x)+function(x+h))/2;
+      x = x + h;
+    }
+
+    return sum * h;
+}
+
+//=================================================================================
+//Обрахунок інтегралу за методом Сімпсона
+
+double simpss_method ( double a, double b, unsigned int n)
+{
+    double h;
+    double x;
+    double sum = 0;
+    unsigned int k;
+
+    h = ( b - a ) / n;
+    x = a;
+
+    for ( int k = 1; k < n-1; k++)
+    {
+      sum = sum + (function(x)+4*function(x+h/2)+function(x+h))/6;
+      x = x + h;
+    }
+
+    return sum * h;
+}
+//=================================================================================
+// Вивід результату
+
+void print_result (double a, double b, unsigned int n, double I)
+{
+    system("cls");
+    printf("\n\n============================================");
+    printf("\n|                Value input               |");
+    printf("\n============================================");
+    printf("\n a=%.2lf b=%.2lf n=%u ", a, b, n );
+    printf("\n\n============================================");
+    printf("\n|                  Result                  |");
+    printf("\n============================================");
+    printf("\n Integral= %.8lf", I);
+    printf("\n============================================");
+
+}
+
+
+//================================================================================
+
